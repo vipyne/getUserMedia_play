a0f12b3ae24f7704f0e5fd330620235874a9eb6b
vanessa <vipyne@gmail.com>
2014-01-13 16:24:15 -0500

    MVP in place :)

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,28 @@
-function hasGetUserMedia() {
-  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia);
-}
+$(document).ready(function(){
+/////////////////////////////
 
-if (hasGetUserMedia()) {
-  // Good to go!
-} else {
-  alert('getUserMedia() is not supported in your browser');
-}
+navigator.getUserMedia  = navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
+
+var video = document.querySelector('video')
+
+$('button.click').on('click', function(event) {
+  console.log('clicked')
+  var errorCallback = function(e) {
+    console.log('you said no. bummer.', e)
+  }
+
+  navigator.getUserMedia({video: true}, function(localMediaStream) {
+    var video = document.querySelector('video');
+    video.src = window.URL.createObjectURL(localMediaStream);
+
+    video.onloadedmetadata = function(e) {
+      // fun stuff to come
+    };
+  }, errorCallback)
+})
+
+/////////////////////////////
+})
