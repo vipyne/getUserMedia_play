1aa75eab6fa91191fcb70f24fba87d13b8e4cfa2
vanessa <vipyne@gmail.com>
2014-01-25 16:18:34 -0500

    fixed catch if no camera

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 /////////////////////////////
 
   navigator.getUserMedia  = navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia ||
-                            navigator.msGetUserMedia;
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia;
 
   var errorCallback = function(e) {
     console.log('you said no. bummer.', e)
@@ -17,21 +17,19 @@ $(document).ready(function(){
     $('button.click').remove()
     $('input').remove()
 
-    // if (navigator.getUserMedia) {
-    navigator.getUserMedia({video: true}, function(localMediaStream) {
-      var video = document.querySelector('video');
-      video.src = window.URL.createObjectURL(localMediaStream);
-      $('.your-video').show()
-
-      // video.onloadedmetadata = function(e) {
-      //   // fun stuff to come
-      // }
-    }, errorCallback)
-
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true}, function(localMediaStream) {
+        var video = document.querySelector('video');
+        video.src = window.URL.createObjectURL(localMediaStream);
+        $('.your-video').show()
+        // video.onloadedmetadata = function(e) {
+        //   // fun stuff to come
+        // }
+        }, errorCallback)
+    } else {
+      document.write("sorry, you don't gots a camera")
+    }
   })
-// } else {
-  // document.write("sorry, you don't gots a camera")
-// }
 
 /////////////////////////////
 })
