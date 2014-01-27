b43a2c62e6320b3ee1a3260c576161385774f5e0
vanessa <vipyne@gmail.com>
2014-01-27 16:36:25 -0500

    let's find out if i can harness this github array o info

---
+++ app/assets/javascripts/githubStreak.js
@@ -0,0 +1,31 @@
+// def github_streak calendar
+
+//   index = -1
+//   streak = 0
+//   until calendar[index][1] == 0
+//     streak += 1
+//     index -= 1
+//     end
+
+
+// end
+
+$(document).ready(function(){
+/////////////////////////////
+
+$.getJSON("https://github.com/users/vipyne/contributions_calendar_data", function(){
+  console.log(data)
+
+})
+
+// $('button.github').on('click', function(){
+//   var index = -1
+//   var myStreak =
+//   for(index = -1; calendar[index][0] !== 0; index -= 1) {
+//     myStreak.push(calendar[index][0])
+//   return myStreak.length
+//   }
+// })
+
+/////////////////////////////
+})
