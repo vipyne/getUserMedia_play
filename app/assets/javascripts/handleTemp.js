b5be0098d251e7c6bdf761020f111ce820611fe0
vanessa <vipyne@gmail.com>
2014-01-26 21:39:39 -0500

    handlebars still broken but pushing to heroku anyway

--- app/assets/javascripts/handleTemp.js
+++ app/assets/javascripts/handleTemp.js
@@ -1,23 +1,29 @@
-$(function() {
-/////////////////////////////
+// $(function() {
+// /////////////////////////////
 
-  $('button.click').on('click', function(event) {
-    // debugger
-    var source = $('#your-name').html()
-    var template = Handlebars.compile(source)
+//   $('button.click').on('click', function(event) {
+//     console.log("wtf")
+//     console.log($('input').val())
+//     debugger
+//     var source = $('#your-name').html()
+//     var template = Handlebars.compile(source)
+//     var data = {
+//           name: $('input').val()
+//         }
+//         $('.name').append(template(data))
 
-    $.ajax({
-      url: "/",
-      type: 'POST',
-      data: { name: "your name"} // this.serialize()
-    })
-      .done(function(data){
-        console.log("data ", data)
-        $('.name').html(template(data))
-    })
-  })
-  // var context = { name: "your name"}
+//     $.ajax({
+//       url: "/",
+//       type: 'GET',
+//       data: { name: "your name"} // this.serialize()
+//     })
+//       .done(function(data){
+//         console.log("data ", data)
+//         $('.name').append(template(data))
+//     })
+//   })
+//   // var context = { name: "your name"}
 
 
-/////////////////////////////
-})
+// /////////////////////////////
+// })
