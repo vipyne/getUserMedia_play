8385ea640003c6b84e78e160902dfac3423675a2
vanessa <vipyne@gmail.com>
2014-01-27 22:15:33 -0500

    messing around w github api... nothing of merit yet

--- app/assets/javascripts/githubStreak.js
+++ app/assets/javascripts/githubStreak.js
@@ -13,19 +13,46 @@
 $(document).ready(function(){
 /////////////////////////////
 
-$.getJSON("https://github.com/users/vipyne/contributions_calendar_data", function(){
-  console.log(data)
+
+$('button.github').on('click', function(){
+  console.log("bam")
+
+// $.getJSON('https://github.com/users/vipyne/contributions_calendar_data', function(data) {
+//   // $("#jsondata").text(JSON.stringify(data));
+//   console.log(data);
+// });
+
+function parseResults(results) {
+            console.log('hello function callback.');
+        }
+
+$.ajax({
+    type: 'POST',
+    url: "https://github.com/users/vipyne/contributions_calendar_data",// + "?callback=?",
+    dataType: "jsonp",
+    jsonp: false,
+    // crossDomain: true,
+    jsonpCallback: 'parseResults',
+    success: function(data, textStatus, jqXHR){
+      console.log("success data",data)
+      },
+    error: function(data){
+      console.log("fail data",data)
+      }
+    })
+
+// .done(function(data){
+//   console.log("data",data)
+// })
 
 })
 
-// $('button.github').on('click', function(){
 //   var index = -1
 //   var myStreak =
 //   for(index = -1; calendar[index][0] !== 0; index -= 1) {
 //     myStreak.push(calendar[index][0])
 //   return myStreak.length
 //   }
-// })
 
 /////////////////////////////
 })
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController <ApplicationController
 
   def index
-
+    github = Github.new login:'vipyne', password: ENV["GITHUB_PASSWORD"]
+    @bam = github.repos.commits.all  'vipyne', 'getVoteText_play'
+    # @bam = github.activity.events.issue  'vipyne', 'getUserMedia_play'
   end
 
 end
