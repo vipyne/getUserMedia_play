1b2da68adf4e979f48494ed75001b73a551a11a0
vipyne <vipyne@gmail.com>
2020-02-26 22:45:34 -0500

    works again- now with more bells and whistles
    - take out jquery
    - rm threejs idea
    - fix getUserMedia api call
    - add video to canvas "filter" fun

--- app.js
+++ app.js
@@ -1,53 +1,96 @@
-$(document).ready(function(){
+;(function(){
 /////////////////////////////
 
-  navigator.getUserMedia  = navigator.getUserMedia ||
+  navigator.getUserMedia = navigator.getUserMedia ||
   navigator.webkitGetUserMedia ||
   navigator.mozGetUserMedia ||
   navigator.msGetUserMedia;
 
-//   (function asciiInit() {
-//   var width = 640
-//   var height  = 480
+  const vid1 = document.getElementById('your-video');
+  const video = document.querySelector('video');
+  const canvas = document.querySelector('canvas');
+  const vid3 = document.getElementById('my-border');
+  const name = document.getElementById('name');
+  const inputName = document.getElementById('input-name');
+  const form = document.getElementById('your-name');
+  const button = document.getElementById('click');
+  const livideo = document.getElementById('li-video');
+  let interval = 0;
 
-//   renderer = new THREE.CanvasRenderer();
-//   renderer.setSize( width, height );
-//   // container.appendChild( renderer.domElement );
-
-//   effect = new THREE.AsciiEffect( renderer );
-//   effect.setSize( width, height );
-//   container.appendChild( effect.domElement );
-
-// }());
-
-  var errorCallback = function(e) {
-    console.log('you said no. bummer.', e)
-  }
-  // var userMedia = navigator.getUserMedia;
-
-  $('.your-video').hide()
-  $('video').hide()
-  $('.my-border').hide()
-
-  $('button.click').on('click', $('form.your-name'), function(event) {
-
-    $('button.click').remove()
-    $('input').hide()
-
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({video: { 'optional': [{ width: 200}, {height: 200 }] } }, function(localMediaStream) {
-        var video = document.querySelector('video');
-        video.src = window.URL.createObjectURL(localMediaStream);
-        $('.your-video').show()
-        $('video').show()
-        $('.my-border').show()
-        // asciiInit()
-        $('.name').append('hi ' + $('.input-name').val() + '!')
-        }, errorCallback)
-    } else {
-      document.write("sorry, you don't gots a camera")
-    }
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    button.click();
   })
 
+  button.addEventListener('click', (event) => {
+    showFrame();
+    getMediaStream();
+    interval = setInterval(function() {
+      videoToCanvas(video, canvas);
+    },100);
+  });
+
+  hideFrame();
+  form.focus();
+  livideo.classList.add('mirror-animation');
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Manipulating_video_using_canvas
+  function videoToCanvas(video, canvas) {
+    let ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, 640, 480);
+    let frame = ctx.getImageData(0, 0, 640, 480);
+    let l = frame.data.length / 4;
+
+    for (let i = 0; i < l; i++) {
+      let r = frame.data[i * 4 + 0];
+      let g = frame.data[i * 4 + 1];
+      let b = frame.data[i * 4 + 2];
+      let a = frame.data[i * 4 + 3];
+      frame.data[i * 4 + 3] = 255/2; // semi transparent
+      if (r < 100 || g > 200) {
+        frame.data[i * 4 + 2] = 255;
+      }
+      if (g < 100 || b > 200) {
+        frame.data[i * 4 + 0] = 255;
+      }
+      if (b < 100 || r > 200) {
+        frame.data[i * 4 + 1] = 255;
+      }
+    }
+    ctx.putImageData(frame, 0, 0);
+  }
+
+  async function getMediaStream() {
+    let stream = null;
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 640, height: 480 }
+      })
+      playStream(stream);
+    } catch(e) {
+      console.log("_____getMediaStream error:", e);
+    }
+  }
+
+  function playStream(stream) {
+    video.srcObject = stream;
+    video.onloadedmetadata = function(event) {
+      video.play();
+    }
+  }
+
+  function showFrame() {
+    vid1.style.display = 'block';
+    video.style.display = 'block';
+    vid3.style.display = 'block';
+    name.append('hi ' + inputName.value + '!')
+  }
+
+  function hideFrame() {
+    vid1.style.display = 'none'; // yellow
+    video.style.display = 'none';
+    vid3.style.display = 'none'; // pink
+  }
+
 /////////////////////////////
-})
+})(window, document);
